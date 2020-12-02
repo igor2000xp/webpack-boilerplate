f17e08672884f2716d30e23c9fe6dac88b828821
Alexey Koren <johnsmitlpf@gmail.com>
2020-12-02 17:24:18 +0300

    feat: update boilerplate

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,8 @@
-import { path } from 'path';
-import { CleanWebpackPlugin } from 'clean-webpack-plugin';
-
-import { MiniCssExtractPlugin } from 'mini-css-extract-plugin';
-
-import { HtmlWebpackPlugin } from 'html-webpack-plugin';
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, options) => {
   const isProduction = options.mode === 'production';
@@ -12,10 +11,16 @@ module.exports = (env, options) => {
     mode: isProduction ? 'production' : 'development',
     devtool: isProduction ? 'none' : 'source-map',
     watch: !isProduction,
-    entry: ['./src/index.js', './src/sass/style.scss'],
+    entry: ['./src/index.js'],
     output: {
       filename: 'bundle.js',
       path: path.join(__dirname, '/build'),
+    },
+    resolve: {
+      extensions: ['.js', '.json', '.mjs'],
+      alias: {
+        '@': path.join(__dirname, 'src'),
+      },
     },
     module: {
       rules: [
@@ -29,9 +34,16 @@ module.exports = (env, options) => {
             },
           },
         }, {
-          test: /\.scss$/,
+          test: /\.(sa|sc|c)ss$/,
           use: [
-            MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader',
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                hmr: process.env.NODE_ENV === 'development',
+              },
+            },
+            'css-loader',
+            'sass-loader',
           ],
         }, {
           test: /\.(png|svg|jpe?g|gif|ttf)$/,
@@ -46,15 +58,19 @@ module.exports = (env, options) => {
         },
       ],
     },
-
+    devServer: {
+      contentBase: './src/static/',
+    },
     plugins: [
-      isProduction ? new CleanWebpackPlugin() : () => {},
+      isProduction ? new CleanWebpackPlugin({}) : () => {},
+      isProduction ? new CopyWebpackPlugin({
+        patterns: [{ from: './src/static', to: '.' }],
+      }) : () => {},
       new HtmlWebpackPlugin({
-        favicon: './src/img/favicon.ico',
         template: './src/index.html',
       }),
       new MiniCssExtractPlugin({
-        filename: 'style.css',
+        filename: './src/style.scss',
       }),
     ],
   };
