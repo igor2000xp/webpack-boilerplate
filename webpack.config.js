f6b8956fdeca6fc58f9ddc227492b32eeca39922
Aleksey Koren <johnsmitlpf@gmail.com>
2021-11-14 18:52:22 +0200

    Update packages, scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,83 @@
 const path = require('path');
+
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer');
 
-module.exports = (env, options) => {
-  const isProduction = options.mode === 'production';
+const nothing = () => {};
 
-  const config = {
-    mode: isProduction ? 'production' : 'development',
-    devtool: isProduction ? 'none' : 'source-map',
-    watch: !isProduction,
-    entry: ['./src/index.js'],
-    output: {
-      filename: 'bundle.js',
-      path: path.join(__dirname, '/build'),
+const isProduction = process.options.mode === 'production';
+const isAnalyze = process.env.analyze;
+
+const config = {
+  mode: isProduction ? 'production' : 'development',
+  devtool: isProduction ? 'none' : 'source-map',
+  watch: !isProduction,
+  entry: ['./src/index.js'],
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, '/build'),
+  },
+  resolve: {
+    extensions: ['.js', '.json', '.mjs'],
+    alias: {
+      '@': path.join(__dirname, 'src'),
     },
-    resolve: {
-      extensions: ['.js', '.json', '.mjs'],
-      alias: {
-        '@': path.join(__dirname, 'src'),
-      },
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /(node_modules)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-            },
-          },
-        }, {
-          test: /\.(sa|sc|c)ss$/,
-          use: [
-            {
-              loader: MiniCssExtractPlugin.loader,
-              options: {
-                hmr: process.env.NODE_ENV === 'development',
-              },
-            },
-            'css-loader',
-            'sass-loader',
-          ],
-        }, {
-          test: /\.(png|svg|jpe?g|gif|ttf)$/,
-          use: {
-            loader: 'file-loader',
-          },
-        }, {
-          test: /\.html$/,
-          use: {
-            loader: 'html-loader',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
           },
         },
-      ],
-    },
-    devServer: {
-      contentBase: './src/static/',
-    },
-    plugins: [
-      isProduction ? new CleanWebpackPlugin({}) : () => {},
-      isProduction ? new CopyWebpackPlugin({
-        patterns: [{ from: './src/static', to: '.' }],
-      }) : () => {},
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-      }),
-      new MiniCssExtractPlugin({
-        filename: './src/style.scss',
-      }),
+      }, {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+          'sass-loader',
+        ],
+      }, {
+        test: /\.(png|svg|jpe?g|gif|ttf)$/,
+        use: {
+          loader: 'file-loader',
+        },
+      }, {
+        test: /\.html$/,
+        use: {
+          loader: 'html-loader',
+        },
+      },
     ],
-  };
-
-  return config;
+  },
+  devServer: {
+    contentBase: './src/static/',
+  },
+  plugins: [
+    isProduction ? new CleanWebpackPlugin({}) : nothing,
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: './src/style.scss',
+    }),
+    isAnalyze ? new BundleAnalyzerPlugin() : nothing,
+    isProduction
+      ? new CopyWebpackPlugin({ patterns: [{ from: './src/static', to: '.' }] })
+      : nothing,
+  ],
 };
+
+export default config;
